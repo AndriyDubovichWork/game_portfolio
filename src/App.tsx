0e193fa383f3a8c533eb3e868465dced2a0bebaf
AndriyDubovichWork <86286088+AndriyDubovichWork@users.noreply.github.com>
2022-12-30 16:07:59 +0200

    add models and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+import { OrbitControls, Stars, useFBX } from '@react-three/drei';
+import { Canvas, useLoader } from 'react-three-fiber';
+
+import { TextureLoader } from 'three/src/loaders/TextureLoader';
+
+const Car = () => {
+  const fbx = useFBX('/assets/cars/Models/car_1.fbx');
+  const colorMap = useLoader(TextureLoader, '/assets/cars/Textures/car_1.png');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <mesh>
+      <primitive object={fbx} />
+      <meshStandardMaterial map={colorMap} attach={'material'} />
+    </mesh>
   );
-}
+};
+
+const App = () => {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.9} />
+      <spotLight position={[10, 15, 10]} angle={0.3} intensity={1} />
+      <OrbitControls />
+      <Car />
+      <Stars />
+    </Canvas>
+  );
+};
 
 export default App;
