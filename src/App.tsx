3f0f53ecc347673ccefb044af1e49f37be167f07
AndriyDubovichWork <86286088+AndriyDubovichWork@users.noreply.github.com>
2022-12-31 23:02:37 +0200

    add car model

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,19 @@ import React from 'react';
 
 import './App.css';
 
-import { OrbitControls, Stars, useFBX } from '@react-three/drei';
-import { Canvas, useLoader } from 'react-three-fiber';
-
-import { TextureLoader } from 'three/src/loaders/TextureLoader';
-
-const Car = () => {
-  const fbx = useFBX('/assets/cars/Models/car_1.fbx');
-  const colorMap = useLoader(TextureLoader, '/assets/cars/Textures/car_1.png');
-  return (
-    <mesh>
-      <primitive object={fbx} />
-      <meshStandardMaterial map={colorMap} attach={'material'} />
-    </mesh>
-  );
-};
-
+import { OrbitControls, Stars } from '@react-three/drei';
+import { Canvas } from 'react-three-fiber';
+import { Car } from './models/Car';
 const App = () => {
-  return (
-    <Canvas>
-      <ambientLight intensity={0.9} />
-      <spotLight position={[10, 15, 10]} angle={0.3} intensity={1} />
-      <OrbitControls />
-      <Car />
-      <Stars />
-    </Canvas>
-  );
+	return (
+		<Canvas>
+			<ambientLight intensity={0.9} />
+			<spotLight position={[10, 15, 10]} angle={0.3} intensity={1} />
+			<OrbitControls />
+			<Car />
+			<Stars />
+		</Canvas>
+	);
 };
 
 export default App;
